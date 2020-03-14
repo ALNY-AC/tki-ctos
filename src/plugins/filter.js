5381dc0a7fa31ea7e988f73d73142c5859ccb58c
cwz346852114 <49225483+cwz346852114@users.noreply.github.com>
2020-03-14 11:46:41 +0800

    commit

--- src/plugins/filter.js
+++ src/plugins/filter.js
@@ -2,29 +2,29 @@ import Vue from 'vue'
 import area from './lib/area'
 
 Vue.filter('arrToStr', function (value) {
-    if (!value) return '';
-    return value.join(',');
+  if (!value) return '';
+  return value.join(',');
 })
 
 
 Vue.filter('areaToStr', function (value) {
-    if (!value) return '';
-    let areaStr = '';
-    try {
+  if (!value) return '';
+  let areaStr = '';
+  try {
 
-        if (typeof area.province_list[value] != 'undefined') {
-            areaStr = area.province_list[value];
-        }
-        if (typeof area.city_list[value] != 'undefined') {
-            areaStr = area.city_list[value];
-        }
-        if (typeof area.county_list[value] != 'undefined') {
-            areaStr = area.county_list[value];
-        }
-    } catch (error) {
-        console.warn(`区号[${value}]不存在`);
+    if (typeof area.province_list[value] != 'undefined') {
+      areaStr = area.province_list[value];
     }
-    return areaStr;
+    if (typeof area.city_list[value] != 'undefined') {
+      areaStr = area.city_list[value];
+    }
+    if (typeof area.county_list[value] != 'undefined') {
+      areaStr = area.county_list[value];
+    }
+  } catch (error) {
+    console.warn(`区号[${value}]不存在`);
+  }
+  return areaStr;
 })
 
 // province_list
--- src/plugins/url.js
+++ src/plugins/url.js
@@ -4,8 +4,8 @@ let Url = {
   // serverUrl: 'http://192.168.0.122:81/admin',//吴杰的本地
   // serverUrl: 'http://kido.house-api.yihuo-cloud.com/admin',//李子龙的本地
   // serverUrl: 'http://127.0.0.1:12193/ctos',//李传浩的本地
-  // serverUrl: 'https://api.house.yihuo-cloud.com/admin',//生产环境
-    serverUrl: 'https://api.flame.yihuo-cloud.cn/ctos',//测试环境
+  serverUrl: 'https://api.flame.yihuo-cloud.com/ctos',//生产环境
+    // serverUrl: 'https://api.flame.yihuo-cloud.cn/ctos',//测试环境
  
   // uploadUrl: '',
   uploadUrl: 'https://api.yihuo-cloud.com',
